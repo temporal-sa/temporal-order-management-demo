e7c7e66b48137fd5079d03c8af273a1010710d48
Donald Forbes <donald.forbes@temporal.io>
2025-03-26 08:00:45 +0000

    Change to startup script so that it uses a parameter to identify the environment or if not set then the TEMPORAL_ENV environment var is used.

--- ui/startcloudwebui.sh
+++ ui/startcloudwebui.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-source ../setcloudenv.sh
+# pass in the temporal environment to use to pick up the connection strings as the first parameter to this script or 
+# ensure that the environment variable TEMPORAL_ENV is set appropriately.
+source ../setcloudenv.sh $1
 
 echo "Starting Web UI on http://localhost:5000 ..."
 poetry run python app.py
