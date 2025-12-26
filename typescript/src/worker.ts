2ce82d4daf4d6e6da2cd71825a46c1d07d29e02d
Peter Sullivan <pvsone@gmail.com>
2025-12-26 15:47:48 -0600

    typescript: implement env config

--- typescript/src/worker.ts
+++ typescript/src/worker.ts
@@ -1,40 +1,42 @@
 import { NativeConnection, Runtime, Worker } from '@temporalio/worker';
 import * as activities from './activities/index';
-import { getWorkflowOptions, getConnectionOptions, getTelemetryOptions, namespace, taskQueue } from './env';
-import { createApiKeyServer } from './apikey-server';
+import { getWorkflowOptions, getTelemetryOptions, taskQueue } from './env';
+import { loadClientConnectConfig } from '@temporalio/envconfig';
+// import { createApiKeyServer } from './apikey-server';
 
 async function main() {
   const telemetryOptions = getTelemetryOptions();
-
   if (telemetryOptions) {
     Runtime.install(telemetryOptions);
   }
 
-  const connectionOptions = await getConnectionOptions();
-  const connection = await NativeConnection.connect(connectionOptions);
+  const config = loadClientConnectConfig();
+  const connection = await NativeConnection.connect(config.connectionOptions);
+  console.info(`✅ Client connected to ${config.connectionOptions.address} in namespace '${config.namespace}'`);
 
-  if (process.env.TEMPORAL_APIKEY) {
-    createApiKeyServer(connection).listen(3333, () => {
-      console.log('API Key server is running on http://localhost:3333');
+  // if (process.env.TEMPORAL_APIKEY) {
+  //   createApiKeyServer(connection).listen(3333, () => {
+  //     console.log('API Key server is running on http://localhost:3333');
+  //   });
+  // }
+
+  try {
+    const worker = await Worker.create({
+      connection,
+      namespace: config.namespace,
+      taskQueue,
+      activities: { ...activities },
+      ...getWorkflowOptions(),
     });
+
+    console.info('🤖: Temporal Worker Online! Beep Boop Beep!');
+    await worker.run();
+  } finally {
+    await connection.close();
   }
-
-  const worker = await Worker.create({
-    connection,
-    namespace,
-    taskQueue,
-    activities: { ...activities },
-    ...getWorkflowOptions(),
-  });
-
-  console.info('🤖: Temporal Worker Online! Beep Boop Beep!');
-  await worker.run();
 }
 
-main().then(
-  () => void process.exit(0),
-  (err) => {
-    console.error(err);
-    process.exit(1);
-  },
-);
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
--- typescript/startlocalworker.sh
+++ typescript/startlocalworker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+export TEMPORAL_ADDRESS=localhost:7233
+export TEMPORAL_NAMESPACE=default
 npm install
 npm run start.watch
