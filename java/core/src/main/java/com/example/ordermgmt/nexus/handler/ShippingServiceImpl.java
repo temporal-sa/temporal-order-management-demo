d7f9ddf69df752205c22a7fd60893a7272c786aa
Peter Sullivan <pvsone@gmail.com>
2025-12-27 15:05:28 -0600

    java: implement auto discovery for nexus service

--- java/core/src/main/java/com/example/ordermgmt/nexus/handler/ShippingServiceImpl.java
+++ java/core/src/main/java/com/example/ordermgmt/nexus/handler/ShippingServiceImpl.java
@@ -8,7 +8,12 @@ import io.nexusrpc.handler.ServiceImpl;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.nexus.Nexus;
 import io.temporal.nexus.WorkflowRunOperation;
+import io.temporal.spring.boot.NexusServiceImpl;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@NexusServiceImpl(taskQueues = "${shipping.task-queue}")
 @ServiceImpl(service = ShippingService.class)
 public class ShippingServiceImpl {
     @OperationImpl
