e719c030e04085010e9cc38b35acd2ac077c10cc
Peter Sullivan <pvsone@gmail.com>
2025-12-27 15:52:46 -0600

    python: implement env config

--- python/startlocalworker.sh
+++ python/startlocalworker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+export TEMPORAL_ADDRESS=localhost:7233
+export TEMPORAL_NAMESPACE=default
 uv run worker.py
--- python/worker.py
+++ python/worker.py
@@ -1,7 +1,8 @@
 import asyncio
 import os
 
-from temporalio.client import Client, TLSConfig
+from temporalio.client import Client
+from temporalio.envconfig import ClientConfig
 from temporalio.worker import Worker
 
 from activities import OrderActivities
@@ -11,26 +12,9 @@ from shipping_child_workflow import ShippingChildWorkflow
 
 
 async def main():
-    address = os.getenv("TEMPORAL_ADDRESS","127.0.0.1:7233")
-    namespace = os.getenv("TEMPORAL_NAMESPACE","default")
-    tlsCertPath = os.getenv("TEMPORAL_TLS_CLIENT_CERT_PATH","")
-    tlsKeyPath = os.getenv("TEMPORAL_TLS_CLIENT_KEY_PATH","")
-    tls = None
-
-    if tlsCertPath and tlsKeyPath:
-        with open(tlsCertPath,"rb") as f:
-            cert = f.read()
-        with open(tlsKeyPath,"rb") as f:
-            key = f.read()
-
-        tls = TLSConfig(client_cert=cert,
-                        client_private_key=key)
-
-    client = await Client.connect(
-        target_host=address,
-        namespace=namespace,
-        tls=tls
-    )
+    connect_config = ClientConfig.load_client_connect_config()
+    client = await Client.connect(**connect_config)
+    print(f"✅ Client connected to {client.service_client.config.target_host} in namespace '{client.namespace}'")
 
     activities = OrderActivities()
 
@@ -52,7 +36,6 @@ async def main():
             activities.undo_charge_customer
         ],
     )
-    print(f"Connecting to Temporal on {address}")
     print("Python order management worker starting...")
     await worker.run()
 
